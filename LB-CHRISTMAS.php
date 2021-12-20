933c80a4e7ccfab61b274fe9fed518d3519dbe53
loubal70 <louisbal70@gmail.com>
2021-12-20 13:51:48 +0100

    MAJ Color

--- LB-CHRISTMAS.php
+++ LB-CHRISTMAS.php
@@ -13,60 +13,61 @@ Version: 1.0.0
 Author URI: https://louis-boulanger.fr
 */
 
+if ( !is_admin() ){
+    // Add style and script
+    wp_enqueue_script('christmas-js', plugin_dir_url(__FILE__) . 'assets/christmas-script.js', array(), null, true);
+    wp_enqueue_style('christmas-css', plugin_dir_url(__FILE__) . '/assets/christmas-style.css', array(), '0.1.0', 'all');
 
-// Add style and script
-wp_enqueue_script('christmas-js', plugin_dir_url(__FILE__) . 'assets/christmas-script.js', array(), null, true);
-wp_enqueue_style('christmas-css', plugin_dir_url(__FILE__) . '/assets/christmas-style.css', array(), '0.1.0', 'all');
 
+    function christmas_light() {
+        echo '
+        <ul class="christmas-lightrope">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        ';
+    }
 
-function christmas_light() {
-    echo '
-    <ul class="christmas-lightrope">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-    ';
+    add_action('wp_head', 'christmas_light');
+
 }
-
-add_action('wp_head', 'christmas_light');
-
