59b99b3bc2426fbd4c7f5a69c48cdb64b872d4f6
loubal70 <louisbal70@gmail.com>
2021-12-20 13:15:26 +0100

    V1.0

---
+++ LB-CHRISTMAS.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @package LB-Christmas
+ * @version 1.0.0
+ */
+/*
+Plugin Name: LB-Christmas
+Plugin URI: https://localhost
+Description: Ajouter de belles animations de noël sur votre site internet
+Author: Louis Boulanger
+Version: 1.0.0
+Author URI: https://louis-boulanger.fr
+*/
+
+
+// Add style and script
+wp_enqueue_script('christmas-js', plugin_dir_url(__FILE__) . 'assets/christmas-script.js', array(), null, true);
+wp_enqueue_style('christmas-css', plugin_dir_url(__FILE__) . '/assets/christmas-style.css', array(), '0.1.0', 'all');
+
+
+function christmas_light() {
+    echo '
+    <ul class="christmas-lightrope">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    ';
+}
+
+add_action('wp_head', 'christmas_light');
+
